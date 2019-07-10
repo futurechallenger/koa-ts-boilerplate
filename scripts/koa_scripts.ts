eec7990bd044391a3b82b443daa31bfa95032820
Bruce Li <bruce.li@ringcentral.com>
2019-07-10 17:56:04 +0800

    download & uncompress

--- scripts/koa_scripts.ts
+++ scripts/koa_scripts.ts
@@ -3,11 +3,13 @@ import Request from 'request';
 import process from 'process';
 import fs from 'fs';
 import path from 'path';
+import decompress from 'decompress';
 
 const uri =
   'https://github.com/futurechallenger/koa-ts-boilerplate/releases/download/v1.0.3/koa-ts-boilerplate_1.0.3_Darwin_i386.tar.gz';
 let basePath = process.cwd();
-const FILE_NAME = 'go-cli';
+const FILE_NAME = 'go-cli.raw';
+const DEST_FILE_PREFIX = '.raw';
 
 function error(message: string): void {
   console.error(message);
@@ -15,7 +17,6 @@ function error(message: string): void {
 
 const download = (uri: string, callback: (destPath: string) => () => void) => {
   const destPath = path.join(basePath, FILE_NAME);
-  console.log('===>dest path: ', destPath);
   Request.head(uri, (err, res, body) => {
     const r = Request(uri).pipe(fs.createWriteStream(destPath));
     r.on('close', callback(destPath));
@@ -24,5 +25,7 @@ const download = (uri: string, callback: (destPath: string) => () => void) => {
 };
 
 download(uri, (destPath: string) => () => {
-  console.log('Download succeeded');
+  console.log('Destination path: ', destPath);
+  const destFilename = destPath.indexOf('.raw');
+  decompress(destPath, 'go-cli').then(files => console.log('decompressed file: ', files));
 });

efb8a0c69be18eb8f7786026b1f2584f90037536
Uncle Charlie <lixiangdong@mobike.com>
2019-07-11 00:18:17 +0800

    parse arguments

--- scripts/koa_scripts.ts
+++ scripts/koa_scripts.ts
@@ -1,16 +1,27 @@
 #!/usr/bin/env node
-import Request from 'request';
-import process from 'process';
-import fs from 'fs';
-import path from 'path';
-import decompress from 'decompress';
-import { FILE } from 'dns';
+import Request from "request";
+import process from "process";
+import fs from "fs";
+import path from "path";
+import decompress from "decompress";
+import { FILE } from "dns";
 
 const uri =
-  'https://github.com/futurechallenger/koa-ts-boilerplate/releases/download/v1.0.3/koa-ts-boilerplate_1.0.3_Darwin_x86_64.tar.gz';
+  "https://github.com/futurechallenger/koa-ts-boilerplate/releases/download/v1.0.3/koa-ts-boilerplate_1.0.3_Darwin_x86_64.tar.gz";
 let basePath = process.cwd();
-const FILE_NAME = 'go-cli';
-const DEST_FILE_PREFIX = '.raw';
+const FILE_NAME = "go-cli";
+const DEST_FILE_PREFIX = ".raw";
+
+const handleExit = () => {
+  console.log("Existing...");
+};
+
+const handleError = () => {
+  console.log("Error");
+};
+
+process.on("SIGINT", handleExit);
+process.on("uncaughtException", handleError);
 
 function error(message: string): void {
   console.error(message);
@@ -20,20 +31,23 @@ const download = (uri: string, callback: (destPath: string) => () => void) => {
   const destPath = path.join(basePath, `${FILE_NAME}${DEST_FILE_PREFIX}`);
   Request.head(uri, (err, res, body) => {
     const r = Request(uri).pipe(fs.createWriteStream(destPath));
-    r.on('close', callback(destPath));
-    r.on('error', error);
+    r.on("close", callback(destPath));
+    r.on("error", error);
   });
 };
 
-download(uri, (destPath: string) => () => {
-  fs.access(path.join(basePath, FILE_NAME), fs.constants.F_OK, err => {
-    if (!err) {
-      fs.unlink(path.join(destPath, FILE_NAME), err => {
-        console.error('Existing files does not removed');
-      });
-    }
-    decompress(destPath, basePath).then(files =>
-      console.log('decompressed file: ', files),
-    );
-  });
-});
+// download(uri, (destPath: string) => () => {
+//   fs.access(path.join(basePath, FILE_NAME), fs.constants.F_OK, err => {
+//     if (!err) {
+//       fs.unlink(path.join(destPath, FILE_NAME), err => {
+//         console.error("Existing files does not removed");
+//       });
+//     }
+//     decompress(destPath, basePath).then(files =>
+//       console.log("decompressed file: ", files)
+//     );
+//   });
+// });
+
+const args = process.argv.slice(2);
+console.log("===>args", args);

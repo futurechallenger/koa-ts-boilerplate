8c81d000e2430975fb552b0591147163ada323eb
Uncle Charlie <lixiangdong@mobike.com>
2019-07-10 22:52:44 +0800

    download and uncompress

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: 'all',
-  arrowParens: 'avoid',
-  parser: 'typescript',
-  singleQuote: true,
-  tabWidth: 2,
-  printWidth: 90,
-};
--- scripts/koa_scripts.ts
+++ scripts/koa_scripts.ts
@@ -4,11 +4,12 @@ import process from 'process';
 import fs from 'fs';
 import path from 'path';
 import decompress from 'decompress';
+import { FILE } from 'dns';
 
 const uri =
-  'https://github.com/futurechallenger/koa-ts-boilerplate/releases/download/v1.0.3/koa-ts-boilerplate_1.0.3_Darwin_i386.tar.gz';
+  'https://github.com/futurechallenger/koa-ts-boilerplate/releases/download/v1.0.3/koa-ts-boilerplate_1.0.3_Darwin_x86_64.tar.gz';
 let basePath = process.cwd();
-const FILE_NAME = 'go-cli.raw';
+const FILE_NAME = 'go-cli';
 const DEST_FILE_PREFIX = '.raw';
 
 function error(message: string): void {
@@ -16,7 +17,7 @@ function error(message: string): void {
 }
 
 const download = (uri: string, callback: (destPath: string) => () => void) => {
-  const destPath = path.join(basePath, FILE_NAME);
+  const destPath = path.join(basePath, `${FILE_NAME}${DEST_FILE_PREFIX}`);
   Request.head(uri, (err, res, body) => {
     const r = Request(uri).pipe(fs.createWriteStream(destPath));
     r.on('close', callback(destPath));
@@ -25,7 +26,14 @@ const download = (uri: string, callback: (destPath: string) => () => void) => {
 };
 
 download(uri, (destPath: string) => () => {
-  console.log('Destination path: ', destPath);
-  const destFilename = destPath.indexOf('.raw');
-  decompress(destPath, 'go-cli').then(files => console.log('decompressed file: ', files));
+  fs.access(path.join(basePath, FILE_NAME), fs.constants.F_OK, err => {
+    if (!err) {
+      fs.unlink(path.join(destPath, FILE_NAME), err => {
+        console.error('Existing files does not removed');
+      });
+    }
+    decompress(destPath, basePath).then(files =>
+      console.log('decompressed file: ', files),
+    );
+  });
 });
